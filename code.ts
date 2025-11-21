9bba30b145ccc7852ad58cb628f2ca033cfffdaa
koki-ueno <koki.ueno@dena.com>
2025-11-21 18:24:04 +0900

    Remove outdated comments

--- code.ts
+++ code.ts
@@ -1,11 +1,3 @@
-// This plugin will open a window to prompt the user to enter a number, and
-// it will then create that many rectangles on the screen.
-
-// This file holds the main code for plugins. Code in this file has access to
-// the *figma document* via the figma global object.
-// You can access browser APIs in the <script> tag inside "ui.html" which has a
-// full browser environment (See https://www.figma.com/plugin-docs/how-plugins-run).
-
 // This shows the HTML page in "ui.html".
 figma.showUI(__html__, { width: 400, height: 600 });
 
