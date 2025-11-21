1a5aeda96d3a86daa99f3baf37e42828f7f48551
koki-ueno <koki.ueno@dena.com>
2025-11-21 13:31:30 +0900

    Enhance Figma plugin functionality by adding element count retrieval and editing features, along with UI updates for better user interaction

--- code.ts
+++ code.ts
@@ -7,31 +7,74 @@
 // full browser environment (See https://www.figma.com/plugin-docs/how-plugins-run).
 
 // This shows the HTML page in "ui.html".
-figma.showUI(__html__);
+figma.showUI(__html__, { width: 300, height: 400 });
+
+// 要素数を取得する関数
+function getElementCount(selectionOnly: boolean): number {
+  if (selectionOnly) {
+    // 選択範囲のみ
+    return figma.currentPage.selection.length;
+  } else {
+    // ファイル全体
+    return figma.currentPage.findAll().length;
+  }
+}
+
+// 要素を取得する関数
+function getElements(selectionOnly: boolean): readonly SceneNode[] {
+  if (selectionOnly) {
+    // 選択範囲のみ
+    return figma.currentPage.selection;
+  } else {
+    // ファイル全体
+    return figma.currentPage.findAll();
+  }
+}
+
+// 要素数をUIに送信
+function sendElementCount(selectionOnly: boolean) {
+  const count = getElementCount(selectionOnly);
+  figma.ui.postMessage({
+    type: 'element-count',
+    count: count,
+    selectionOnly: selectionOnly
+  });
+}
+
+// 初期要素数を送信
+sendElementCount(false);
+
+// 選択が変更されたときに要素数を更新
+figma.on('selectionchange', () => {
+  // チェックボックスの状態を確認するために、UIに問い合わせる必要がある
+  // ここでは簡易的に、選択が変更されたら両方の状態を送信する
+  // 実際の実装では、UIから現在のチェックボックス状態を保持する必要がある
+  figma.ui.postMessage({ type: 'selection-changed' });
+});
 
 // Calls to "parent.postMessage" from within the HTML page will trigger this
 // callback. The callback will be passed the "pluginMessage" property of the
 // posted message.
-figma.ui.onmessage =  (msg: {type: string, count: number}) => {
-  // One way of distinguishing between different types of messages sent from
-  // your HTML page is to use an object with a "type" property like this.
-  if (msg.type === 'create-shapes') {
-    // This plugin creates rectangles on the screen.
-    const numberOfRectangles = msg.count;
-
-    const nodes: SceneNode[] = [];
-    for (let i = 0; i < numberOfRectangles; i++) {
-      const rect = figma.createRectangle();
-      rect.x = i * 150;
-      rect.fills = [{ type: 'SOLID', color: { r: 1, g: 0.5, b: 0 } }];
-      figma.currentPage.appendChild(rect);
-      nodes.push(rect);
+figma.ui.onmessage = (msg: { type: string; selectionOnly?: boolean }) => {
+  if (msg.type === 'get-element-count') {
+    // 要素数を取得してUIに送信
+    sendElementCount(msg.selectionOnly || false);
+  } else if (msg.type === 'edit') {
+    // 編集ボタンが押されたとき
+    const elements = getElements(msg.selectionOnly || false);
+    
+    // 該当する要素のX座標をすべて0にする
+    for (const node of elements) {
+      if ('x' in node) {
+        node.x = 0;
+      }
     }
-    figma.currentPage.selection = nodes;
-    figma.viewport.scrollAndZoomIntoView(nodes);
+    
+    // 編集後も要素数を更新
+    sendElementCount(msg.selectionOnly || false);
+    
+    // UIを閉じない（figma.closePlugin()を呼ばない）
+  } else if (msg.type === 'selection-changed') {
+    // UIから選択変更を通知された場合（実際には使わないが、将来の拡張用）
   }
-
-  // Make sure to close the plugin when you're done. Otherwise the plugin will
-  // keep running, which shows the cancel button at the bottom of the screen.
-  figma.closePlugin();
 };
